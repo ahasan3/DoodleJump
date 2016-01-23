ebf49285534969821a8b9a3704c145ce22bda0b6
Anthony Cruz <anthonycruz34@gmail.com>
2016-01-22 23:15:58 -0500

    Holds all the constants

---
+++ Constants.java
@@ -0,0 +1,26 @@
+package DoodleJump;
+
+
+
+public class Constants {
+	
+	public static final int GRAVITY = 1000; // acceleration constant (UNITS: pixels/s^2)
+	public static final int REBOUND_VELOCITY = -900; // initial jump velocity (UNITS: pixels/s^2)
+	public static final int DURATION = 18; // KeyFrame duration (UNITS: ms)
+
+	public static final int PLATFORM_WIDTH = 40; // platform width (UNITS: pixels)
+	public static final int PLATFORM_HEIGHT = 10; // platform height (UNITS: pixels)
+	public static final int DOODLE_WIDTH = 20; // doodle width (UNITS: pixels)
+	public static final int DOODLE_HEIGHT = 40; // doodle height (UNITS: pixels)
+
+	public static final int GAMEWIDTH = 300;
+	public static final int GAMEHEIGHT = 460;
+	public static final int LABELWIDTH = 400;
+	public static final int LABELHEIGHT= 100;
+	public static final int BUTTONSPACING = 15;
+	
+	public static final int DOODLESTARTX = 180;
+	public static final int DOODLESTARTY = 400;
+	public static final int PLATFORMSTARTX = 170;
+	public static final int PLATFORMSTARTY = 440;
+}
